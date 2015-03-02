590b480c224edfe9dae3ef80186de798074fae9b
Ruby Shrestha <ruby.shrestha@deerwalk.edu.np>
2015-03-02 13:54:35 +0545

    Update main.cpp
    Changes made to array and made and used object pointer

--- Inventory Project/main.cpp
+++ Inventory Project/main.cpp
@@ -9,12 +9,30 @@ using namespace std;
 int main(){
 	//Inventory obj1("Tresemme", "Shampoo", 50);
 	Inventory obj[3];
-	obj[0].setItemName("Tresemme");
-	obj[0].setItemCategory("Shampoo");
-	obj[0].setItemStock(50);
-	obj[0].purchase(8);
-	obj[0].sales(4);
-	obj[0].lost(1);
-	obj[0].damaged(1);
-	obj[0].displayStockMessage();
+	Inventory obj1("Tresemme", "Shampoo", 10);
+	Inventory *ptr_obj2;
+	Inventory obj2;
+	ptr_obj2=&obj2;
+	
+	obj1.purchase(8);
+	obj1.lost(10);
+	obj1.damaged(8);
+	
+	if (!(obj1.isStockEmpty()))
+		obj1.sales(4);
+	else
+		cout << "Stock Empty! Cannot sell.";
+	
+	obj1.purchase(2);
+
+	arrobj[0]=obj1;
+	arrobj[0].displayStockMessage();
+	
+	ptr_obj2->setItemName("Detol");
+	ptr_obj2->setItemCategory("Hand Sanitizer");
+	ptr_obj2->setItemStock(3);
+	ptr_obj2->purchase(2);
+	
+	arrobj[1]=*ptr_obj2;
+	arrobj[1].displayStockMessage();
 }
